e604d6d6d72c119e9123c6a3204e88f91cc542ed
ayush23719 <91633771+ayush23719@users.noreply.github.com>
2022-05-14 01:32:45 +0530

    news

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
-
 import React, { Component } from 'react'
+import Navbar from './Navbar';
+import bootstrap from 'bootstrap';
 
 export default class App extends Component {
   render() {
     return (
-      <div>Class Based Component~</div>
+      <Navbar></Navbar>
     )
   }
 }
