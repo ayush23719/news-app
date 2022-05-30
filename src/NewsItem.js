c01e8ad00428a39ee7a042c3857bb91687305b65
ayush23719 <91633771+ayush23719@users.noreply.github.com>
2022-05-30 23:22:13 +0530

    dynamic cards

--- src/NewsItem.js
+++ src/NewsItem.js
@@ -5,7 +5,7 @@ export class NewsItem extends Component {
     let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div className="card my-3" style={{ width: "18rem" }}>
-        <img src={imageUrl} className="card-img-top" alt="..." />
+        <img src={!imageUrl ? "https://climate.onep.go.th/wp-content/uploads/2020/01/default-image.jpg" : imageUrl} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{title}...</h5>
           <p className="card-text">{description}...</p>
